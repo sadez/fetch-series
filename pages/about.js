a839dec2202683476a2e995970ed0b9057a603eb
sadez <salim.aboubakri@gmail.com>
2018-02-24 17:58:31 +0000

    add nextauth

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { NextAuth } from 'next-auth/client'
+import Router from 'next/router'
+import Link from 'next/link'
 //mui imports
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
@@ -17,14 +20,33 @@ import Layout from '../components/MyLayout.js'
 //About page ./page
 class About extends Component {
 
+  static async getInitialProps({req}) {
+    return {
+      session: await NextAuth.init({req})
+    }
+  }
 
   constructor (props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSignOutSubmit = this.handleSignOutSubmit.bind(this)
+
     this.state = {
      checkedA: true
    };
   };
+
+  handleSignOutSubmit(event) {
+    event.preventDefault()
+    NextAuth.signout()
+    .then(() => {
+      Router.push('/auth/callback')
+    })
+    .catch(err => {
+      Router.push('/auth/error?action=signout')
+    })
+  }
+
 
     handleChange(event) {
        this.setState({checkedA: event.target.checked});
@@ -35,6 +57,8 @@ class About extends Component {
     return(
       <Layout>
         <List className="listStyle">
+          <SignInMessage {...this.props}/>
+
           <Card raised = {true} className="">
                 <CardContent>
                   <Grid container direction='row' justify='center' alignItems='center'>
@@ -85,6 +109,29 @@ class About extends Component {
          </List>
       </Layout>
     );
+  }
+}
+
+
+export class SignInMessage extends React.Component {
+  render() {
+    if (this.props.session.user) {
+      return (
+        <React.Fragment>
+          <p><Link href="/auth"><a className="btn btn-secondary">Manage Account</a></Link></p>
+          <form id="signout" method="post" action="/auth/signout" onSubmit={this.handleSignOutSubmit}>
+            <input name="_csrf" type="hidden" value={this.props.session.csrfToken}/>
+            <button type="submit" className="btn btn-outline-secondary">Sign out</button>
+          </form>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <p><Link href="/auth"><a className="btn btn-primary">Sign in</a></Link></p>
+        </React.Fragment>
+      )
+    }
   }
 }
 
